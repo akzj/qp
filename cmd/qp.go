4389b1ad7e1c2ab9a3bfd7f0fc022b9dafb1d5bc
akzj <fu.niukey@gmail.com>
2020-12-08 23:53:05 +0800

    add cmd

--- cmd/qp.go
+++ cmd/qp.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"qp"
+)
+
 func main() {
-	$END$
+	file := flag.String("file", "", "--file example.qp")
+	flag.Parse()
+
+	if *file == "" {
+		fmt.Println("require source file to execute")
+		os.Exit(1)
+		return
+	}
+	data, err := ioutil.ReadFile(*file)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+		return
+	}
+	qp.Parse(string(data)).Invoke()
 }
